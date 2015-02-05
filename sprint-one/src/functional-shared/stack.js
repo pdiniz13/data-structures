95807afed544f8595190fb64cc76dea10709a875
Paulo Diniz <pdiniz_100@yahoo.com>
2015-02-05 11:45:04 -0800

    modified stack to remove redundant use of storage

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,9 +2,7 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = {
-    storage:{
-      end:0
-    }
+    end:0
   };
   _.extend(someInstance, stackMethods);
   return someInstance
@@ -12,17 +10,17 @@ var Stack = function() {
 
 var stackMethods = {
   push: function(value){
-    this.storage[this.storage.end++] = value;
+    this[this.end++] = value;
   },
   pop: function(){
-    if(this.storage.end>0){
-      var value = this.storage[--this.storage.end];
-      delete this.storage[this.storage.end];
+    if(this.end>0){
+      var value = this[--this.end];
+      delete this[this.end];
       return value;
     }
   },
   size: function(){
-    return this.storage.end;
+    return this.end;
   }
 };
 
