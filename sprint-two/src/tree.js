b061a5f7aeafdd37008a3eca9ada75be14b1f5da
Paulo Diniz <pdiniz_100@yahoo.com>
2015-02-06 15:56:05 -0800

    completed tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,32 @@
 var Tree = function(value){
   var newTree = {};
+  _.extend(newTree, treeMethods);
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
-
+  var returnValue = false;
+  var recursiveFunction = function(target, position) {
+    if (position.value === target) {
+      returnValue = true;
+      return;
+    }
+    if(position.children !== undefined) {
+      for (var i = 0, count = position.children.length; i < count; i++) {
+        recursiveFunction(target, position.children[i]);
+      }
+    }
+  };
+  recursiveFunction(target, this);
+  return returnValue;
 };
 
 /*
