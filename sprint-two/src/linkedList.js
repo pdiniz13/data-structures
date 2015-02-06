329b2ae8072507af86ab30e8e15ec7521a37f254
Paulo Diniz <pdiniz_100@yahoo.com>
2015-02-06 12:05:17 -0800

    finished linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,33 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value){
+  list.addToTail = function(value) {
+    var newNode = Node(value);
+    if(list.head === null){
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function(){
+    var returnValue = list.head.value;
+    list.head = list.head.next;
+    return returnValue;
   };
 
   list.contains = function(target){
+    var current = list.head;
+    var bool = false;
+    while(current !== null){
+      if(current.value === target){
+        bool = true;
+      }
+      current = current.next;
+    }
+    return bool;
   };
 
   return list;
@@ -26,4 +46,6 @@ var Node = function(value){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * Linear for contains, constant for the rest.
  */
+//
