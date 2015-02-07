710c617e3d592dee8225e7f0b29ed3314f8f5461
Paulo Diniz <pdiniz_100@yahoo.com>
2015-02-06 17:08:41 -0800

    completed graph, and fixed graph spec test

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,38 @@
-
-
 var Graph = function(){
 };
 
 Graph.prototype.addNode = function(node){
+  this[node] = [];
 };
 
 Graph.prototype.contains = function(node){
+  return this[node] !== undefined;
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  return this[fromNode].indexOf(toNode) !== -1;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this[fromNode].push(toNode);
+  this[toNode].push(fromNode);
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  this[fromNode].splice(this[fromNode].indexOf(toNode), 1);
+  this[toNode].splice(this[fromNode].indexOf(fromNode), 1);
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for(var node in this){
+    if(Array.isArray(this[node])) {
+      cb(node);
+    }
+  }
 };
 
 /*
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -31,4 +31,6 @@ treeMethods.contains = function(target){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * add is constant. Contains is linear.
  */
+
