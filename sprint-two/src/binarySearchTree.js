c30432fe985a5db2d93d3b5a020c3909e7f63ebe
Paulo Diniz <pdiniz_100@yahoo.com>
2015-02-07 12:36:52 -0800

    binary search is now complete

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,61 @@
 var BinarySearchTree = function(value){
+  this.left = null;
+  this.right = null;
+  this.value = value;
+  this.parent = null;
 };
 
+BinarySearchTree.prototype.insert = function(value){
+  if(this.value > value){
+    if(this.left === null){
+      this.left = new BinarySearchTree(value);
+    } else {
+     this.left.insert(value);
+    }
+  } else {
+    if(this.right === null) {
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function(value){
+  var bool = false;
+
+  this.each(function(){
+    if(this.value === value){
+      bool = true;
+    }
+  });
+  return bool;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb){
+  cb(this.value);
+  if(this.left !== null){
+    this.left.depthFirstLog(cb);
+    //cb.apply(this.left, arguments);
+
+  }
+  if(this.right !== null){
+    this.right.depthFirstLog(cb);
+    //cb.apply(this.right, arguments);
+  }
+};
+
+
+BinarySearchTree.prototype.each = function(cb){
+  cb(this.value);
+  if(this.left !== null){
+    cb.apply(this.left, arguments);
+
+  }
+  if(this.right !== null){
+    cb.apply(this.right, arguments);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
